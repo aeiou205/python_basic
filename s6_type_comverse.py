8672360375d2516d9a41a757d3255f90f9a3bcd0
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-06-08 23:33:45 -0500

    Add files via upload

---
+++ s6_type_comverse.py
@@ -0,0 +1,15 @@
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926" 
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string) + 1
+pi2_string = pi_string + "das"
+print(pi_float)
+print(pi2_string)
