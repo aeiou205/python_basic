9b108a01d6d7986126bb19c0434df0cf87835e63
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-06-08 20:56:37 -0500

    Update python_types.py

--- python_types.py
+++ python_types.py
@@ -1,10 +1,24 @@
 # Create a variable savings
 print("this program is to python types")
+#float
 v1=5.0
 # Print out savings
 print( v1 )
 
 #create a variable saving witn te value 100
+#string
 v2='daniel' 
 #check out this variable by typing print(saving) in the 
 print( v2 )
+
+# Create a new string desc, witb the value 
+print("values string")
+v3 = 'aeiou'
+v4 = "205"
+print( v3 )
+
+# Create a new boolean, profitable, with the value true
+print("values falses or true ")
+v1 = True
+v2 = False
+print (v2)
