adc85a9a40c0499bece9f00cddc5f25cb24e04d2
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-06-08 23:32:13 -0500

    Add files via upload
    comvertion betwen types data

---
+++ s5python_types.py
@@ -0,0 +1,13 @@
+# Create a variable savings
+print("this program is to python types")
+v1=5.0
+
+# Print out savings
+print( v1 )
+
+# create a variable saving witn te value 100
+v2='daniel' 
+
+# check out this variable by typing print(saving) in the 
+print( v2 )
+
